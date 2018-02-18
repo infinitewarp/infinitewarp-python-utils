337a405af2fb94feb005a2c50b562192f4fa1648
Brad Smith <bradster@infinitewarp.com>
2018-02-18 13:50:05 -0500

    don't assert exact execution times in tests

--- tests/test_timing.py
+++ tests/test_timing.py
@@ -14,8 +14,8 @@ def test_timer_context_manager():
 def test_timer_context_manager_check_while_running():
     """Assert timing.Timer gives elapsed time if checked while running."""
     with timing.Timer() as timer:
-        expected_print = 'still timing; currently 0.0000 sec'
-        assert str(timer) == expected_print
+        expected_print = 'still timing; currently'
+        assert str(timer).startswith(expected_print)
 
 
 def test_timer_context_manager_verbose():
@@ -23,8 +23,8 @@ def test_timer_context_manager_verbose():
     with mock.patch.object(timing, 'print') as mock_print:
         with timing.Timer(action='foo', verbose=True):
             pass
-        expected_print = '0.0000 sec for foo'
-        mock_print.assert_called_with(expected_print)
+        expected_print = 'sec for foo'
+        mock_print.mock_calls[0][1][0].endswith(expected_print)
 
 
 def test_timer_object():
@@ -39,11 +39,11 @@ def test_timed_decorator():
     """Assert time.timed decorator produces expected output."""
     with mock.patch.object(timing, 'print') as mock_print:
         expected_print = 'func:funky args:[(True,), {\'foo\': \'bar\'}] ' \
-                         'took: 0.0000 sec'
+                         'took: '
 
         @timing.timed
         def funky(*args, **kwargs):
             pass
 
         funky(True, foo='bar')
-        mock_print.assert_called_with(expected_print)
+        mock_print.mock_calls[0][1][0].startswith(expected_print)
